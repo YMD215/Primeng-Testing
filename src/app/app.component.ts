2b99d3f46ef90ba351ea560af0324ff912dee5dc
YOUSSEF <ymdht215@gmail.com>
2025-02-12 00:58:46 +0200

    first init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ProductService } from './service/productservice';
+import { TableModule } from 'primeng/table';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [TableModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Primeng-Testing';
+  products!: any[];
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit() {
+    this.productService.getProductsMini().then((data) => {
+      this.products = data;
+    });
+  }
 }

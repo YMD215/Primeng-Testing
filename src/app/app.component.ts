8feb70decb1fd4db13a2e810066feb26a198e37d
YOUSSEF <ymdht215@gmail.com>
2025-07-06 03:09:26 +0300

    testing toaster

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,54 @@
-import { Component } from '@angular/core';
-import { ProductService } from './service/productservice';
-import { TableModule } from 'primeng/table';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TableModule } from 'primeng/table';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
+import { ProductService } from './service/productservice';
 
 @Component({
   selector: 'app-root',
-  imports: [TableModule, CommonModule],
+  standalone: true,
+  imports: [CommonModule, TableModule, ToastModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers: [MessageService]
 })
 export class AppComponent {
   title = 'Primeng-Testing';
-  products!: any[];
+  products: any[] = [];
+
+  private messageService = inject(MessageService);
 
   constructor(private productService: ProductService) {}
 
   ngOnInit() {
+    this.showError('Test error message');
     this.productService.getProductsMini().then((data) => {
       this.products = data;
     });
+  }
+
+  showError(details: string | string[] | undefined) {
+    if (!details) return;
+
+    if (Array.isArray(details)) {
+      details.forEach((detail) => {
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Error',
+          detail: detail,
+        });
+      });
+    } else {
+      this.messageService.add({
+        severity: 'error',
+        summary: 'Error',
+        detail: details,
+      });
+    }
+  }
+  showMsg(){
+    console.log('show msg')
+    this.showError('error ya basha')
   }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,19 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { routes } from './app.routes';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideAnimations(),
+    provideAnimationsAsync(),
     providePrimeNG({
       theme: { preset: Aura },
-    }),    
-  ]
+    }),
+  ],
 };
